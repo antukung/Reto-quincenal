6ecbaca5dc4c9143c3590781daa557db587194a5
Antu <122485403+antukung@users.noreply.github.com>
2023-04-28 22:44:48 -0300

    aca esta la segunda parte
    GRACIAS TOTALES

--- algoritmos.js
+++ algoritmos.js
@@ -1,12 +1,24 @@
 const inputUsuario= document.querySelector("#nombre-ingresado");
+const inputedad= document.querySelector ("#edad");
+const inputemail= document.querySelector ("#email");
+const inputdni =document.querySelector ("#dni");
 const parrafo1= document.querySelector ("#parrafo1");
+const parrafo2= document.querySelector ("#parrafo2");
+const parrafo3= document.querySelector ("#parrafo3");
+const parrafo4= document.querySelector ("#parrafo4");
 
 function verifica() {
     
 let valorDeUsuario = inputUsuario.value;
+let valorDeEdad = inputedad.value;
+let valorDeEmail = inputemail.value;
+let valorDedDNI= inputdni.value;
 
-parrafo1.innerHTML="Hola bienvenido"+valorDeUsuario     
 
+parrafo1.innerHTML="Hola bienvenido "+valorDeUsuario;     
+parrafo2.innerHTML= "Que bueno que tienes "+valorDeEdad;
+parrafo3.innerHTML= "Ya reguistramos tu email "+valorDeEmail;
+parrafo4.innerHTML= "Que lindo numero de DNI= " +valorDedDNI;
 
 }
 
