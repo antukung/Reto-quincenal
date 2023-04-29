6f247f48a02c0d9c0b666471c14d9748aba098b4
Antu <122485403+antukung@users.noreply.github.com>
2023-04-28 21:21:18 -0300

    no me sale
    no me sale un carajo

---
+++ algoritmos.js
@@ -0,0 +1,12 @@
+const inputUsuario= document.querySelector(" #nombre-ingresado ");
+const parrafoUno= document.querySelector ("#parrafo1");
+
+function verifica() {
+    
+let valorDeUsuario = inputUsuario.value;
+
+parrafoUno.innerHTML("Hola bienvenido"+valorDeUsuario);     
+
+
+}
+
